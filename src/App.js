9012d392101bfe208ec38cc7a10ed604ab0be35e
puremoru <e155745@ie.u-ryukyu.ac.jp>
2021-01-30 23:13:05 +0900

    :+1: App Componentにpropsで渡す

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super();
+    this.title = props.title;
+    this.message = props.message;    
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>React</h1>
+        <p>{this.title}</p>
+        <p>{this.message}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
